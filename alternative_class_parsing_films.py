5135342ddd90ae90d5046f0f3831458b417421ad
Pavel Isaev <isaev.pavelalex@gmail.com>
2026-01-13 17:53:10 +0300

    add alternative_parsing.py and authentication(register and login)

---
+++ alternative_class_parsing_films.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import textwrap
+from urllib.parse import urljoin
+BASE_URL = "https://24afisha.by"
+
+class ParsingFilms:
+    def __init__(self):
+        self.url = BASE_URL
+        self.response = requests.get(self.url)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
+        self.list_li = None
+        self.get_li()
+
+    def get_li(self):
+        elm_ul = self.soup.find("ul", class_="events__list")
+        self.list_li = elm_ul.find_all("li")
+        # for ul in uls:
+        #     parent = ul.parent
+        #     if parent and parent.get("id") == "events-block":
+        #         for li in ul.find_all("li"):
+        #             self.list_li.append(li)
+
+    def get_title(self):
+        result = ["Список фильмов:"]
+        for li in self.list_li:
+            title = li.find_all("a", class_="name")[0].get_text(strip=True)
+            result.append(f"Название: {title}")
+        return result
+
+    def get_films(self):
+        result = []
+        # for film in self.list_li:
+        #     print(film.prettify())
+        #     print('----------------')
+        for li in self.list_li:
+            result.append({"title": li.find("img").get("alt"),
+                           "url": urljoin(BASE_URL, li.find("a").get("href")),
+                           "image": li.find("img").get("src")})
+        return result
+
+    def get_image(self):
+        result = ["\nСсылки на картинки:"]
+        for li in self.list_li:
+            picture = li.find_all("a", class_="media")[0]
+            title = picture.find("img").get("alt").strip()
+            img_src = picture.find("img").get("src")
+            result.append(f"{title}\n{img_src}")
+        return result
+
+    def get_description(self):
+        result = ["\nОписание фильмов:"]
+        for li in self.list_li[:10]:
+            response = requests.get(li.find_all("a", class_="name")[0].get("href"))
+            soup = BeautifulSoup(response.text, "lxml")
+            title = li.find_all("a", class_="name")[0].get_text(strip=True)
+            description = soup.find_all(class_="b-article post")[0].get_text(strip=True)
+            result.append(f"Название: {title}\nОписание: {textwrap.fill(description, width=205)}\n")
+        return result
--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request, session, redirect, jsonify
 from datetime import datetime
 from class_parsing_currencies import ParsingCurrency
-from class_parsing_films import ParsingPosterFilms
+# from class_parsing_films import ParsingFilms
+from alternative_class_parsing_films import ParsingFilms
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = 'secret_key_for_session'  # обязательно для session
+users = {}
+films = ParsingFilms()
+films_row = films.get_films()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    films = ParsingPosterFilms()
-    films_row = films.get_films()
     error_message = None
+    username = session.get("username")
 
     if 'currency_data' not in session:
         currency = ParsingCurrency()
@@ -53,13 +57,90 @@ def index():
         FILMS=films_row,
         DATE=date,
         CURRENCY=currency_data,
-        ERROR=error_message
+        ERROR=error_message,
+        USERNAME=username
     )
 
 
-@app.route('/reset')
-def reset_currency():
+@app.route('/login', methods=['POST'])
+def login():
+    login_error = None
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username or not password:
+        login_error = "Заполните форму"
+
+    elif username in users:
+        input_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        if users[username] == input_hash:
+            session["username"] = username
+        else:
+            login_error = "Invalid password"
+    else:
+        login_error = "Invalid username or password"
+
+    date = session.get('date')
+    currency_data = session.get('currency_data')
+
+    if "username" in session:
+        return render_template(
+            "index.html",
+            USERNAME=session["username"],
+            FILMS=films_row,
+            DATE=date,
+            CURRENCY=currency_data,
+            LOGIN_ERROR=login_error
+        )
+    return render_template(
+        "index.html",
+        FILMS=films_row,
+        DATE=date,
+        CURRENCY=currency_data,
+        LOGIN_ERROR=login_error
+    )
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    register_error = None
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if not username or not password:
+        register_error = "Заполните форму!"
+    elif not username in users:
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        users[username] = password_hash
+        register_error = f"{username} регистрация прошла успешно"
+    else:
+        register_error = "Такой логин уже существует"
+    date = session.get('date')
+    currency_data = session.get('currency_data')
+    return render_template(
+        "index.html",
+        REGISTER_ERROR=register_error,
+        FILMS=films_row,
+        DATE=date,
+        CURRENCY=currency_data
+    )
+
+
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    return render_template("main.html")
+
+
+@app.route('/logout')
+def logout():
     session.clear()
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.pop('currency_data', None)
     return redirect('/')
 
 
