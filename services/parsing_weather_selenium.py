c014f44f17621d3cfd5a2206780ac2ac62d72e5f
Pavel Isaev <isaev.pavelalex@gmail.com>
2026-02-12 21:17:22 +0300

    refactoring deploy

--- services/parsing_weather_selenium.py
+++ services/parsing_weather_selenium.py
@@ -20,7 +20,7 @@ def parsing_weather(city_name=None):
         options.binary_location = "/usr/bin/chromium"
         service = Service("/usr/bin/chromedriver")
 
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")  # иногда нужно
