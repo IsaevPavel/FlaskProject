78ba8e6a3e4a7f5c8fa260ef2e1ef08696105d53
Pavel Isaev <isaev.pavelalex@gmail.com>
2026-02-12 21:31:52 +0300

    refactoring deploy

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+# gunicorn app:app
+import os
 
-options = Options()
-options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-
-service = Service(r"C:\PythonProject\chromedriver.exe")  # путь к настоящему chromedriver
-
-driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://www.gismeteo.by/weather-minsk-4248/")
-print(driver.title)  # проверка, что страница загрузилась
-driver.quit()
+print("Chromium exists:", os.path.exists("/usr/bin/chromium"))
+print("Chromedriver exists:", os.path.exists("/usr/bin/chromedriver"))
